b7e747b6ebeb865b1366437e3de5105edb3e687a
Heo-seongmu <gjtjdan0624@gmail.com>
2025-11-05 21:30:15 +0900

    도시 검색 서비스 수정
    엔티티에 컬럼명 연결 시켜줘서 매핑 잘 되게 해줌

--- src/main/java/hsm/bootproject/SearchFlight/domain/basicArea.java
+++ src/main/java/hsm/bootproject/SearchFlight/domain/basicArea.java
@@ -1,5 +1,6 @@
 package hsm.bootproject.SearchFlight.domain;
 
+import jakarta.persistence.Column; // 1. 이 import 문을 꼭 추가해주세요!
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -9,19 +10,28 @@ import lombok.ToString;
 
 @Entity
 @Getter
-@Setter@ToString
+@Setter
+@ToString
 public class basicArea {
 
 	@Id
 	@GeneratedValue
 	private Long id;
 	
+    // ▼ 2. @Column 어노테이션을 추가하여 DB 컬럼 이름을 명시합니다.
+	@Column(name = "KOLOCATION") 
 	private String kolocation;
 	
+    // ▼ 3. @Column 어노테이션 추가
+	@Column(name = "ENLOCATION")
 	private String enlocation;
 	
+    // ▼ 4. @Column 어노테이션 추가
+	@Column(name = "IATA_CODE")
 	private String iataCode;
 	
+    // ▼ 5. @Column 어노테이션 추가 (가장 중요!)
+	@Column(name = "COUNTRY")
 	private String country;
 	
 	public basicArea() {
